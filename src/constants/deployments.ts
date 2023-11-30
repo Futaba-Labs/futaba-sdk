a65b38bdc8061aac3a997cc8b5e28fb63fbc7668
adachi_440 <tomoki1999314@gmail.com>
2023-11-30 15:17:57 +0400

    update address

--- src/constants/deployments.ts
+++ src/constants/deployments.ts
@@ -9,7 +9,7 @@ export const GATEWAY: Record<ChainStage, Partial<Record<ChainKey, string>>> = {
   },
   [ChainStage.TESTNET]: {
     [ChainKey.GOERLI]: "",
-    [ChainKey.MUMBAI]: "0x3fab87824ABbe2DC686ed5CbB032d9c62E2fe179",
+    [ChainKey.MUMBAI]: "0x01E0698e24fD0ea124ED539Bc8570b68548061C5",
     [ChainKey.ARBITRUM_GOERLI]: "",
     [ChainKey.OPTIMISM_GOERLI]: "",
   },
@@ -30,13 +30,13 @@ export const LIGHT_CLIENT: Record<ChainStage, Partial<Record<ChainKey, string>>>
   },
   [ChainStage.TESTNET]: {
     [ChainKey.GOERLI]: "",
-    [ChainKey.MUMBAI]: "0x8EDfB284e0b406F2e8C46De28A313688A762E373",
+    [ChainKey.MUMBAI]: "0x87bbEfB934B52Dd329f26ccb7B903eEbc4d0c6b8",
     [ChainKey.ARBITRUM_GOERLI]: "",
     [ChainKey.OPTIMISM_GOERLI]: "",
   },
   [ChainStage.DEVNET]: {
     [ChainKey.GOERLI]: "",
-    [ChainKey.MUMBAI]: "0x8EDfB284e0b406F2e8C46De28A313688A762E373",
+    [ChainKey.MUMBAI]: "0x87bbEfB934B52Dd329f26ccb7B903eEbc4d0c6b8",
     [ChainKey.ARBITRUM_GOERLI]: "",
     [ChainKey.OPTIMISM_GOERLI]: "",
   }
